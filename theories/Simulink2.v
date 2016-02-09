06202f747b9322ee893e24d1d5eab72252e1c209
Daniel Ricketts <daricket@cs.ucsd.edu>
2016-02-09 12:48:52 -0800

    Some updates to thermometer proof.

--- theories/Simulink2.v
+++ theories/Simulink2.v
@@ -54,7 +54,7 @@ Proof.
   - intros. field.
 Qed.
 
-Definition StateFlow {I O S : Type}
+Definition StateFlow_ClosedRight {I O S : Type}
            (trans : S -> I -> S)
            (out : S -> I -> O)
            (init : S)
@@ -68,20 +68,23 @@ Definition StateFlow {I O S : Type}
                forall t' : R, t < t' <= t + epsilon ->
                  st t' = trans (st t) (i t)).
 
+Definition StateFlow_ClosedLeft {I O S : Type}
+           (trans : S -> I -> S)
+           (out : S -> I -> O)
+           (init : S)
+: Arrow I O :=
+  fun i o =>
+    exists st : Signal S,
+         st 0 = init
+      /\ (forall t : R, out (st t) (i t) = o t)
+      /\ (forall t : R, 0 < t ->
+             exists epsilon : R, epsilon > 0 /\
+               forall t' : R, t - epsilon <= t' < t ->
+                 st t = trans (st t') (i t)).
+
 Definition Switch {T} (i1 i2 : Signal T)
            (test : Signal bool) (o : Signal T) : Prop :=
   forall t, o t = if test t then i1 t else i2 t.
-
-Definition thermostat (init : R) (temp : Signal R)
-           (on : Signal bool) (dtemp : Signal R) :=
-  Switch (fun _ => 1) (fun _ => -1) on dtemp /\
-  Integrator init dtemp temp /\
-  StateFlow (I:=R) (S:=bool) (O:=bool)
-            (fun s i => if s
-                        then if Rge_dec i 1 then false else true
-                        else if Rle_dec i (-1) then true else false)
-            (fun s _ => s)
-            true dtemp on.
 
 Definition is_lower_bound (P : R -> Prop) (m : R) : Prop :=
   forall x : R, P x -> m <= x.
@@ -119,29 +122,35 @@ Require Import Psatz.
 Theorem real_induction :
   forall P : R -> Prop,
     P 0 ->
-    (forall x : R, (forall y : R, 0 <= y < x -> P y) -> P x) ->
-    (forall x : R, P x -> exists y, x < y /\ forall z, x < z < y -> P z) ->
+    (forall x : R, 0 < x ->
+                   (forall y : R, 0 <= y < x -> P y) -> P x) ->
+    (forall x : R,
+        0 <= x ->
+        P x ->
+        exists y, x < y /\ forall z, x < z < y -> P z) ->
     forall x : R, 0 <= x -> P x.
 Proof.
   intros P Base I1 I2.
-  cut (~exists x : R, 0 <= x /\ ~P x).
+  cut (~exists x : R, 0 < x /\ ~P x).
   - intros. apply not_ex_all_not with (n:=x) in H.
-    apply not_and_or in H. destruct H; [ tauto | ].
-    apply NNPP; assumption.
+    apply not_and_or in H. destruct H.
+    + assert (x = 0) by psatzl R.
+      subst; assumption.
+    + apply NNPP; assumption.
   - intro Hnot.
-    assert (lower_bounded (fun x => 0 <= x /\ ~P x)) as Hlb.
+    assert (lower_bounded (fun x => 0 < x /\ ~P x)) as Hlb.
     { unfold lower_bounded, is_lower_bound. exists 0.
-      intros. tauto. }
-    pose proof (glb (fun x => 0 <= x /\ ~P x) Hlb Hnot) as Hglb.
+      intros. psatzl R. }
+    pose proof (glb (fun x => 0 < x /\ ~P x) Hlb Hnot) as Hglb.
     destruct Hglb as [xglb Hglb]. unfold is_glb in *.
     assert (0 <= xglb).
-    { assert (is_lower_bound (fun x => 0 <= x /\ ~P x) 0).
-      { unfold is_lower_bound. intros. tauto. }
+    { assert (is_lower_bound (fun x => 0 < x /\ ~P x) 0).
+      { unfold is_lower_bound. intros. psatzl R. }
       intuition. }
     assert (P xglb \/ ~P xglb) by apply classic.
     destruct H0.
-    + specialize (I2 xglb H0). destruct I2.
-      assert (is_lower_bound (fun x => 0 <= x /\ ~P x) x).
+    + specialize (I2 xglb H H0). destruct I2.
+      assert (is_lower_bound (fun x => 0 < x /\ ~P x) x).
       { unfold is_lower_bound in *. intros.
         assert (xglb <= x0) by (apply Hglb; assumption).
         destruct H3; [ | subst; tauto ].
@@ -150,19 +159,184 @@ Proof.
         tauto. }
       assert (x <= xglb) by (apply Hglb; assumption).
       psatzl R.
-    + specialize (I1 xglb). apply H0. apply I1. intros.
-      apply NNPP. intro.
+    + assert (0 < xglb).
+      { destruct H; auto. subst. tauto. }
+      specialize (I1 xglb). apply H0. apply I1; auto.
+      intros. apply NNPP. intro.
       assert (xglb <= y).
-      { unfold is_lower_bound in *. apply Hglb. tauto. }
+      { unfold is_lower_bound in *. apply Hglb. split.
+        { destruct H2. destruct H2; auto. subst. tauto. }
+        { assumption. } }
       psatzl R.
+Qed.
+
+(*
+Definition ind_inv1 (dtemp : Signal R) (t : R) :=
+  (dtemp t = -1 \/ dtemp t = 1).
+
+Definition ind_inv2 (temp dtemp : Signal R) (t : R) :=
+  (dtemp t = 1 -> -1 <= temp t < 1) /\
+  (dtemp t = -1 -> -1 < temp t <= 1).
+
+Definition ind_inv (temp dtemp : Signal R) (t : R) :=
+  ind_inv1 dtemp t /\ ind_inv2 temp dtemp t.
+
+Lemma ind_inv_safe :
+  forall temp dtemp t,
+    ind_inv temp dtemp t -> safe temp t.
+Proof.
+  compute. intros. psatzl R.
+Qed.
+*)
+
+Definition Controller (temp : Signal R) (on : Signal bool) :=
+  StateFlow_ClosedLeft
+    (I:=R) (S:=bool) (O:=bool)
+    (fun s i => if s
+                then if Rge_dec i 1 then false else true
+                else if Rle_dec i (-1) then true else false)
+    (fun s _ => s)
+    true temp on.
+
+Definition thermostat (init : R) (temp : Signal R)
+           (on : Signal bool) (dtemp : Signal R) :=
+  Switch (fun _ => 1) (fun _ => -1) on dtemp /\
+  Integrator init dtemp temp /\
+  Controller temp on.
+
+Definition ind_inv_Controller (temp : R) (on : bool) : Prop :=
+  (on = true /\ temp < 1) \/
+  (on = false /\ -1 < temp).
+
+Definition ind_inv_Integrator (dtemp temp : R) : Prop :=
+  (dtemp >= 0 /\ -1 <= temp) \/
+  (dtemp <= 0 /\ temp <= 1).
+
+Definition safe (temp : R) : Prop :=
+  -1 <= temp <= 1.
+
+Lemma ind_inv_safe :
+  forall (temp dtemp : Signal R) (on : Signal bool),
+    Switch (fun _ => 1) (fun _ => -1) on dtemp /\
+    (forall t, 0 <= t -> ind_inv_Controller (temp t) (on t)) /\
+    (forall t, 0 <= t -> ind_inv_Integrator (dtemp t) (temp t)) ->
+    forall t, 0 <= t -> safe (temp t).
+Proof.
+  cbv beta iota zeta delta - [ Rge Rle ]. intros. intuition;
+  specialize (H t); specialize (H1 t); specialize (H3 t);
+  intuition; rewrite H2 in *; psatzl R.
+Qed.
+
+Ltac specialize_arith H :=
+  repeat match type of H with
+         | ?P -> _ =>
+           let X := fresh in
+           assert P as X by psatzl R;
+             specialize (H X); clear X
+         end.
+
+Ltac destruct_ite :=
+  match goal with
+  | [ |- context [ if ?e then _ else _ ] ]
+    => destruct e
+  end.
+
+Lemma ind_inv_Controller_ok :
+  forall (temp : Signal R) (on : Signal bool),
+    temp 0 < 1 ->
+    Controller temp on ->
+    forall t, 0 <= t -> ind_inv_Controller (temp t) (on t).
+Proof.
+  cbv beta iota zeta delta - [ Rge Rle ]. intros temp on Hinit Hst.
+  destruct Hst as [st [Hst0 [Hout Htrans]]]. intros.
+  eapply real_induction
+  with (P:=fun t => (on t = true /\ temp t < 1) \/
+                    (on t = false /\ -1 < temp t)).
+  - rewrite <- Hout. rewrite Hst0. intuition psatzl R.
+  - intros. specialize (Htrans x H0).
+    destruct Htrans as [eps [Heps Htrans]].
+    specialize (Htrans (x - eps)). specialize_arith Htrans.
+    rewrite <- Hout. rewrite Htrans.
+    repeat destruct_ite;
+      intuition psatzl R.
+  - (* This case is unnecessary if we assume axiom of choice. *)
+    admit.
+  - assumption.
+Admitted.
+
+Lemma ind_inv_Integrator_ok :
+  forall (dtemp temp : Signal R),
+    Integrator 0 dtemp temp ->
+    forall t, 0 <= t -> ind_inv_Integrator (dtemp t) (temp t).
+Proof.
+  (* Need something like differential induction. *)
+Admitted.
+
+Lemma Integrator_init :
+  forall s1 s2 init,
+    Integrator init s1 s2 ->
+    s2 0 = init.
+Proof.
+  unfold Integrator. intros. tauto.
 Qed.
 
 Lemma thermostat_in_range :
   forall temp on dtemp,
     thermostat 0 temp on dtemp ->
-    forall t, -1 <= temp t <= 1.
+    forall t, 0 <= t -> safe (temp t).
 Proof.
-  cbv beta iota zeta
-      delta - [ Rge Rle derive_pt derivable_pt ].
+  unfold thermostat. intros temp on dtemp Htherm t Ht.
+  destruct Htherm as [Hswitch [Hint Hcontrol]].
+  eapply ind_inv_safe; [ | assumption ]. split; [ | split ].
+  - eassumption.
+  - intros. apply ind_inv_Controller_ok.
+    + apply Integrator_init in Hint; psatzl R.
+    + eassumption.
+    + assumption.
+  - apply ind_inv_Integrator_ok; assumption.
+Qed.
+(*
   intros temp on dtemp [Hswitch [Hint Hst]] t.
+  cut (ind_inv temp dtemp t); [ apply ind_inv_safe | ].
+  cbv beta iota zeta
+      delta - [ Rge Rle derive_pt derivable_pt ] in *.
+  split.
+  { specialize (Hswitch t). destruct (on t); tauto. }
+  destruct Hst as [st [Hst0 [Hst_out Hst]]].
+  eapply real_induction with (P:=ind_inv2 temp dtemp);
+    cbv beta iota zeta delta - [ Rge Rle ].
+  - specialize (Hst_out 0). specialize (Hswitch 0).
+    rewrite Hst0 in *. rewrite <- Hst_out in *. rewrite Hswitch.
+    psatzl R.
+  - intros. split; intros.
+    + specialize (Hst x H). destruct Hst as [eps [Heps Hst]].
+      specialize (Hst (Rmax 0 (x - eps))).
+      match goal with
+      | H : ?P -> _ |- _ =>
+        match type of P with
+        | Prop =>
+          let X := fresh in
+          assert P as X by admit ;
+            specialize (H X)
+        end
+      end.
+      repeat rewrite Hst_out in *; clear Hst_out.
+      repeat rewrite Hswitch in *.
+      destruct (on x) eqn:?; try solve [ psatzl R ].
+      destruct (on (Rmax 0 (x - eps))) eqn:?.
+      { destruct (Rge_dec (temp x) 1); try congruence.
+        split; [| psatzl R].
+        specialize (H0 (Rmax 0 (x - eps))).
+        destruct H0. admit.
+        rewrite Hswitch in *.
+        rewrite Heqb0 in *.
+        specialize (H0 eq_refl).
+        setoid_rewrite Hswitch in Hint.
+        destruct Hint.
+        destruct H5.
+        admit. }
+      { destruct (Rle_dec (temp x) (-1)); try congruence.
+        
+        
 Admitted.
+*)
